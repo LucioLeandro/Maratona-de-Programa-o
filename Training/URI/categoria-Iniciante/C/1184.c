f93c418ac3be922c60a62d415a6dc744392f661a
Lucio Leandro <lucio.leo85@gmail.com>
2017-09-17 03:52:30 -0300

    adicionando soluções e problema

---
+++ Training/URI/categoria-Iniciante/C/1184.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main() {
+	float matriz[12][12];
+	float soma = 0;
+	int count = 0;
+	char op;
+	fflush(stdin);
+	scanf("%c",&op);
+	int i,j;
+	for(i = 0; i < 12; i++) {
+		for(j = 0; j < 12; j++) {
+			scanf("%f",&matriz[i][j]);
+		if(j < i) {
+				soma += matriz[i][j];
+				count++;
+			}
+		}
+	}
+	float media;
+	if(op == 'S') {
+		printf("%.1f\n", soma);
+	} else if(op == 'M'){
+		media = soma/12;
+		printf("%.1f\n", soma/count);
+	}
+}
