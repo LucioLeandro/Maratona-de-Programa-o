677c8b861b76b13c20b5b3245a2aab5e80c7c73f
Lucio Leandro <lucio.leo85@gmail.com>
2017-09-28 00:19:28 -0300

    adicionando solucao

---
+++ Training/URI/categoria-Iniciante/java/1189.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double[][] matriz = new double[12][12];
+		String op;
+
+
+		op = scanner.nextLine();
+		double soma = 0;
+		int count = 0;
+		for(int i = 0; i < 12; i++) {
+			for(int j = 0; j < 12; j++) {
+				matriz[i][j] = scanner.nextDouble();
+				if(j < i && j < 11-i) {
+					soma += matriz[i][j];
+					count++;
+				}
+			}
+		}
+
+		if(op.equals("S")) {
+			System.out.printf("%.1f\n", soma);
+		} else {
+			System.out.printf("%.1f\n", soma/count);
+		}
+	}
+}
