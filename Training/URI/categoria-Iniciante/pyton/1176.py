5b24b02385786d763c4f6034adb6758f5b7820ef
Lúcio Leandro <lucio.leo85@gmail.com>
2019-05-27 21:27:55 -0300

    1176 resolvido em python

--- Training/URI/categoria-Iniciante/pyton/1176.py
+++ Training/URI/categoria-Iniciante/pyton/1176.py
@@ -4,24 +4,31 @@ vetor = [0, 1]
 
 
 def get_fibonacci(sequencia):
-    position = sequencia
+    sequencia
 
-    if position == 0 or position == 1:
-        print vetor[position]
+    if sequencia == 0 or sequencia == 1:
+        print "Fib(%d) = %d" % (sequencia, vetor[sequencia])
         return
-    
+
     if sequencia < len(vetor):
-        print vetor[sequencia - 1]
+        print "Fib(%d) = %d" % (sequencia, vetor[sequencia])
         return
-    
-    
+
+    contador = len(vetor)
+
+    while contador <= sequencia:
+        vetor.append(vetor[contador - 1] + vetor[contador - 2])
+        contador += 1
+
+    print ("Fib(%d) = %d" % (sequencia, vetor[sequencia]))
+
 
 def main():
     qtd_casos = input()
-    
+
     count = 1
 
-    while(count <= int(qtd_casos)):
+    while(count <= qtd_casos):
         sequencia = input()
         get_fibonacci(sequencia)
         count += 1
